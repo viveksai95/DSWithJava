0ed528e068b396f44c05eeb78e13449601c5ba80
Vivek Krishna Sai <viveksai95@gmail.com>
2021-03-16 23:35:24 +0530

    DFS Traversal Additions

--- src/edu/bvks/binarytress/Tree.java
+++ src/edu/bvks/binarytress/Tree.java
@@ -23,6 +23,8 @@ public class Tree {
 	/**
 	 * Inserts the value in the Binary Search Tree
 	 * 
+	 * Complexity - O(log(n))
+	 * 
 	 * @param value
 	 */
 
@@ -59,6 +61,8 @@ public class Tree {
 	/**
 	 * Checks if the given value is present in the tree and returns back the status
 	 * 
+	 * Complexity - O(log(n))
+	 * 
 	 * @param value
 	 * @return isFound
 	 */
@@ -83,9 +87,70 @@ public class Tree {
 		return false;
 	}
 
+	/**
+	 * Pre-order Traversal for B.S.T => ROOT, LEFT, RIGHT
+	 */
+	public void traversePreOrder() {
+		this.traversePreOrder(root);
+	}
+
+	private void traversePreOrder(Node root) {
+
+		if (root == null)
+			return;
+
+		System.out.println(root.value);
+		traversePreOrder(root.leftChild);
+		traversePreOrder(root.rightChild);
+
+	}
+
+	/**
+	 * In-order Traversal for B.S.T => LEFT, ROOT, RIGHT
+	 */
+
+	public void traverseInOrder() {
+		this.traverseInOrder(root);
+	}
+
+	private void traverseInOrder(Node root) {
+
+		if (root == null)
+			return;
+
+		traverseInOrder(root.leftChild);
+		System.out.println(root.value);
+		traverseInOrder(root.rightChild);
+
+	}
+
+	/**
+	 * Post-order Traversal for B.S.T => LEFT, RIGHT, ROOT
+	 */
+
+	public void traversePostOrder() {
+		this.traversePostOrder(root);
+	}
+
+	private void traversePostOrder(Node root) {
+
+		if (root == null)
+			return;
+
+		traversePostOrder(root.leftChild);
+		traversePostOrder(root.rightChild);
+		System.out.println(root.value);
+
+	}
+
 	// TODO: Note down all the edge cases and check the working functionality of the
 	// code.
 	// TODO: To implement generic Tree Interface, accepting all data types
 	// TODO: To implement delete operation for B.S.T Later.
+	// NOTE: In B.S.T the IN-ORDER-TRAVERSAL gives list of all the elements in
+	// ascending order
+	// Made a blunder mistake, after copy pasting pre to post and in, forgot to
+	// change recursive function names, which totally wasted my time. make sure not
+	// to repeat this bro
 
 }
--- src/edu/bvks/driver/BinaryTreeDriver.java
+++ src/edu/bvks/driver/BinaryTreeDriver.java
@@ -8,22 +8,36 @@ public class BinaryTreeDriver {
 
 		Tree tree = new Tree();
 
-		tree.insert(1);
-		tree.insert(2);
-		tree.insert(3);
-		tree.insert(4);
-		tree.insert(5);
-		tree.insert(6);
-		tree.insert(7);
-		tree.insert(8);
-		tree.insert(9);
-		tree.insert(10);
+//		tree.insert(1);
+//		tree.insert(2);
+//		tree.insert(3);
+//		tree.insert(4);
+//		tree.insert(5);
+//		tree.insert(6);
+//		tree.insert(7);
+//		tree.insert(8);
+//		tree.insert(9);
+//		tree.insert(10);
 
+		tree.insert(7);
+		tree.insert(4);
+		tree.insert(9);
+		tree.insert(1);
+		tree.insert(6);
+		tree.insert(8);
+		tree.insert(10);
+		
 		System.out.println(tree.find(11)); // false
 		System.out.println(tree.find(1)); // true
 		System.out.println(tree.find(5)); // true
 		System.out.println(tree.find(60)); // false
 
+		System.out.println("PRE ORDER");
+		tree.traversePreOrder();
+		System.out.println("IN ORDER");
+		tree.traverseInOrder();
+		System.out.println("POST ORDER");
+		tree.traversePostOrder();
 	}
 
 }
